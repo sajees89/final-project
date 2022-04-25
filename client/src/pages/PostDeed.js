f0653565a5515c59331c5ee7a22b1501e59d5cec
idjh263 <idjh263@gmail.com>
2022-04-24 21:19:26 -0400

    post frontend updated

--- client/src/pages/PostDeed.js
+++ client/src/pages/PostDeed.js
@@ -1,55 +1,80 @@
-// import React from 'react';
-// import { useState} from "react";
-// import { useMutation } from '@apollo/client';
-// import { ADD_POST} from '../../utils/mutations';
-// import { QUERY_POSTS, QUERY_ME } from '../../utils/queries';
+import React from 'react';
+import { useState} from "react";
+import { useMutation } from '@apollo/client';
+import { ADD_POST} from '../utils/mutations';
+import { QUERY_POSTS, QUERY_ME } from '../utils/queries';
 
-//   const PostDeed = () => {
-//     const PostForm = () => {
-//       const [postText, setText] = useState('');
-//       onst [addPost, { error }] = useMutation(ADD_POST, {
-//         update(cache, { data: { addPost } }) {
-//           try {
-//             // update post array's cache
-//             // could potentially not exist yet, so wrap in a try/catch
-//             const { posts } = cache.readQuery({ query: QUERY_POSTS});
-//             cache.writeQuery({
-//               query: QUERY_POSTS,
-//               data: { posts: [addPost, ...posts] },
-//             });
-//           } catch (e) {
-//             console.error(e);
-//           }
+  const PostDeed = () => {
+    const [postText, setText] = useState('');
+      const [addPost, { error }] = useMutation(ADD_POST, {
+        update(cache, { data: { addPost } }) {
+          try {
+            // update post array's cache
+            // could potentially not exist yet, so wrap in a try/catch
+            const { posts } = cache.readQuery({ query: QUERY_POSTS});
+            cache.writeQuery({
+              query: QUERY_POSTS,
+              data: { posts: [addPost, ...posts] },
+            });
+          } catch (e) {
+            console.error(e);
+          }
     
-//           // update me object's cache
-//           const { me } = cache.readQuery({ query: QUERY_ME });
-//           cache.writeQuery({
-//             query: QUERY_ME,
-//             data: { me: { ...me, posts: [...me.posts, addPost] } },
-//           });
-//         },
-//       });
-//       const handleChange = (event) => {  
+          // update me object's cache
+          const { me } = cache.readQuery({ query: QUERY_ME });
+          cache.writeQuery({
+            query: QUERY_ME,
+            data: { me: { ...me, posts: [...me.posts, addPost] } },
+          });
+        },
+      });
+      const handleChange = (event) => {
+        const { name, value } = event.target;
+        setText({...postText, [name]: value });
+      };
+    
+     // submit form
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-//     return (
-//       <div className="create-post">
-//         <div className="post-container">
-//           <h1>Post A Good Deed</h1>
+    try {
+      await addPost({
+        variables: { postText },
+      });
+
+      // clear form value
+      setText('');
+      
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+    return (
+      
+      <div className="create-post">
+        <div className="post-container">
+          <h1>Post A Good Deed</h1>
+          <div className="post-body">
+          <form className="post-form" 
+          onSubmit={handleSubmit}>
+            <h5> Share Your Story :</h5>
+            <textarea
+              placeholder="Share your story"
+              value={postText}
+              className="form-input col-12 col-md-9"
+              onChange={handleChange}
+        ></textarea>
          
-//           <div className="post-body">
-//             <label> Share Your Story :</label>
-//             <textarea
-//               placeholder="Post your story"
-//               // onChange={(event) => {
-//               //   setPostText(event.target.value);
-//               // }}
-//             />
-//           </div>
-//           <button className='btn' type='submit'> Submit Post</button>
-//         </div>
-//       </div>
-//     );
-//   }
+          <button className='btn' type='submit'> Submit Post</button>
+          </form>
+          {error && <div>Post failed</div>}
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
   
 
-// export default PostDeed;
+export default PostDeed;
