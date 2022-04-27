a55a069e8ba58550e3733d795d8ba1fea1543a0a
idjh263 <idjh263@gmail.com>
2022-04-27 19:43:44 -0400

    update

--- client/src/pages/ReadAll.js
+++ client/src/pages/ReadAll.js
@@ -1,40 +1,38 @@
-import React from 'react';
-import Postlist from '../components/Postlist';
-import Postform from '../components/Postform';
+import React from "react";
+import Postlist from "../components/Postlist";
+import Postform from "../components/Postform";
 
-
-import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
-import { QUERY_POSTS } from '../utils/queries';
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/client";
+import { QUERY_POSTS } from "../utils/queries";
+import '../App.css';
 
 const ReadAll = () => {
   const { loading, data } = useQuery(QUERY_POSTS);
-  
+
   const posts = data?.posts || [];
 
   const loggedIn = Auth.loggedIn();
 
   return (
-    <main>
-      <div className="flex-row justify-space-between">
-        {loggedIn && (
-          <div className="col-12 mb-3">
-            <Postform />
-          </div>
-        )}
-        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <Postlist
-              posts={posts}
-              title="Some Feed for Post(s)..."
-            />
+    <div className="p-listing">
+      <main className="card-container">
+        <div className="card">
+          {loggedIn && (
+            <div className="col-12 mb-3">
+              <Postform />
+            </div>
           )}
+          <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <Postlist posts={posts} title="Some Feed for Post(s)..." />
+            )}
+          </div>
         </div>
-        
-      </div>
-    </main>
+      </main>
+    </div>
   );
 };
 
