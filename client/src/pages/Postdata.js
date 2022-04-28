522e0daedb308bc40cf7201ce8a7c6db7e5bc1fc
idjh263 <idjh263@gmail.com>
2022-04-27 21:09:05 -0400

    update

--- client/src/pages/Postdata.js
+++ client/src/pages/Postdata.js
@@ -3,17 +3,31 @@ import { Link } from "react-router-dom";
 import Postform from '../components/Postform'
 import Auth from "../utils/auth";
 
-
+const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
 
 function Postdata() {
   return (
     <div>
-    <Link to="/login">Login</Link>
-    <Link to="/signup">SignUp</Link>
+  
 
-    <div>
-    {Auth.loggedIn() && <Postform/>}
-    <Postform/>
+    <div className='postdata'>
+    {Auth.loggedIn()? (
+            <div className='profile-container'>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </div>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">SignUp</Link>
+              <Postform/>
+            </>
+          )}
     </div>
     </div>
   )
